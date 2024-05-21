7cca2896e9495cec811320a9e454f0db85b09efd
SparklePh <85722233+SparklePh@users.noreply.github.com>
2024-05-21 22:41:52 +0700

    fix mean_absolute_error

--- tool/train_model_true_dataset.py
+++ tool/train_model_true_dataset.py
@@ -1,27 +1,23 @@
 from matrix_factorization import BaselineModel, KernelMF, train_update_test_split
 
 import pandas as pd
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error,mean_absolute_error
 import numpy as np
 import math
-
+import pickle
 
 # Read movie data 
 movie_data = pd.read_csv("tool/dataset/ratings.csv",usecols=[0, 1, 2])
 
 # Rename 
 movie_data = movie_data.rename(columns={"userId": "user_id", "movieId": "item_id"})
-
 movie_data = movie_data.dropna()
-#print(movie_data)
-# set data value to int
-#movie_data = movie_data.astype(np.int8)
 
-
+# Prepare data for online learning
 X = movie_data[["user_id", "item_id"]]
 y = movie_data["rating"]
 
-# Prepare data for online learning
+
 (
     X_train_initial,
     y_train_initial,
@@ -42,9 +38,19 @@ matrix_fact.update_users(
 )
 pred = matrix_fact.predict(X_test_update)
 rmse = mean_squared_error(y_test_update, pred, squared=False)
+mae = mean_absolute_error(y_test_update, pred)
 print(f"\nTest RMSE: {rmse:.4f}")
+print(f"Test MASE: {mae:.4f}")
 
 # # # Get recommendations
-user = 100
+user = 50
 items_known = X_train_initial.query("user_id == @user")["item_id"]
-print(matrix_fact.recommend(user=user, items_known=items_known))
+matrix_fact_data = matrix_fact.recommend(user=user, items_known=items_known)
+print(matrix_fact_data)
+
+# save the model to disk
+model_file = 'models/matrix_factorization_model_0v1.sav'
+pickle.dump(matrix_fact, open(model_file, 'wb'))
+
+data_file = 'models/items_known.sav'
+pickle.dump(X, open(data_file, 'wb'))
